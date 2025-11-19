b288a905ba31225e5975af1952f02e0c0fe6767a
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-11-19 16:54:42 +0200

    Ref to db

--- src/app.py
+++ src/app.py
@@ -1,23 +1,30 @@
-from flask import Flask, redirect, request, flash
+from flask import redirect, request, render_template
 from util import validate_book
 from repositories.reference_repository import create_book
-
-app = Flask(__name__)
+from config import app, test_env
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
-@app.route('/create_book', methods=["POST"])
-def reference_creation():
+@app.route("/new_reference")
+def new():
+    return render_template("new_reference.html")
+
+@app.route("/create_book", methods=["POST"])
+def book_creation():
+    name = request.form.get("name")
     author = request.form.get("author")
     title = request.form.get("title")
     year = request.form.get("year")
+    editor = request.form.get("editor")
+    publisher = request.form.get("publisher")
+    note = request.form.get("note")
 
     try:
         validate_book(author, title, year)
-        create_book(author, title, year)
+        create_book(name, author, title, year, editor, publisher, note)
         return redirect("/")
     except Exception as error:
-        flash(str(error))
+        print(error)
         return redirect("/new_reference")
--- src/repositories/reference_repository.py
+++ src/repositories/reference_repository.py
@@ -1,2 +1,17 @@
-def create_book(author, title, year):
-    pass
+from config import db
+from sqlalchemy import text
+
+def create_book(name, author, title, year, editor, publisher, note):
+    sql = text("INSERT INTO sources (title, author, year, publisher, note) VALUES (:title, :author, :year, :publisher, :note)")
+    db.session.execute(sql, {"title": title,
+                             "author": author,
+                             "year": year,
+                             "publisher": publisher,
+                             "note": note })
+    db.session.commit()
+
+def print_all_titles():
+    result = db.session.execute(text("SELECT title FROM sources"))
+    titles = result.fetchall()
+    for row in titles:
+        print(row.title)
