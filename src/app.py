20c3910bbb2ac26a22b9755c4dcdf8540e0dd6f4
heikkilotjonen <heikkimlotjonen@gmail.com>
2025-11-20 10:35:02 +0200

    links to references work properly in the frontpage

--- src/app.py
+++ src/app.py
@@ -15,26 +15,33 @@ def index():
         reference = {'id': item[0], 'name': item[1]}
         references.append(reference)
 
-    return render_template("index.html", references=references)
+    return render_template('index.html', references=references)
 
-@app.route("/new_reference")
+@app.route('/new_reference')
 def new():
-    return render_template("new_reference.html")
+    return render_template('new_reference.html')
 
-@app.route("/create_book", methods=["POST"])
+@app.route('/reference/<int:reference_id>')
+def show_reference(reference_id):
+    sql = text('SELECT * FROM sources WHERE id = :id')
+    query = db.session.execute(sql, {'id': reference_id})
+    reference = query.fetchone()
+    return render_template('show_reference.html', reference=reference) if reference else redirect('/')
+
+@app.route('/create_book', methods=['POST'])
 def book_creation():
-    name = request.form.get("name")
-    author = request.form.get("author")
-    title = request.form.get("title")
-    year = request.form.get("year")
-    editor = request.form.get("editor")
-    publisher = request.form.get("publisher")
-    note = request.form.get("note")
+    name = request.form.get('name')
+    author = request.form.get('author')
+    title = request.form.get('title')
+    year = request.form.get('year')
+    editor = request.form.get('editor')
+    publisher = request.form.get('publisher')
+    note = request.form.get('note')
 
     try:
         validate_book(author, title, year)
         create_book(name, author, title, year, editor, publisher, note)
-        return redirect("/")
+        return redirect('/')
     except Exception as error:
         print(error)
-        return redirect("/new_reference")
+        return redirect('/new_reference')
