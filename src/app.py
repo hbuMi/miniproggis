f9cd619dc3fc3117c72ae3ff641cb99a33c8b425
heikkilotjonen <heikkimlotjonen@gmail.com>
2025-12-01 23:10:59 +0200

    added search function

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ def index():
     for item in items_books:
         reference = {'id': item[0], 'name': item[1], 'type': 'book'}
         references.append(reference)
-    
+
     for item in items_articles:
         reference = {'id': item[0], 'name': item[1], 'type': 'article'}
         references.append(reference)
@@ -38,10 +38,11 @@ def show_reference(ref_type, reference_id):
     if ref_type == 'book':
         sql = text('SELECT * FROM books WHERE id = :id')
     elif ref_type == 'article':
-        sql = text('SELECT * FROM articles WHERE id = id:')
+        sql = text('SELECT * FROM articles WHERE id = :id')
 
     query = db.session.execute(sql, {'id': reference_id})
     reference = query.fetchone()
+
     return render_template('show_reference.html', reference=reference) if reference else redirect('/')
 
 @app.route('/create_book', methods=['POST'])
@@ -61,6 +62,23 @@ def book_creation():
     except Exception as error:
         flash(str(error))
         return redirect('/new_reference')
+
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    results = []
+    if query:
+        sql = text("""
+        SELECT id, title, 'book' as type
+        FROM books
+        WHERE title ILIKE :query
+        UNION
+        SELECT id, title, 'article' as type
+        FROM articles WHERE title ILIKE :query
+        ORDER BY title
+        """)
+        results = db.session.execute(sql, {"query": f"%{query}%"}).fetchall()
+    return render_template('search.html', query=query, results=results)
 
 @app.route('/reset_db')
 def reset_database():
